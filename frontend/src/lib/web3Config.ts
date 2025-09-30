59392472c3ea96df416a7131a777e9c82c8ae39b
marcdhi <gandhi.mardav@gmail.com>
2025-09-30 17:04:59 +0530

    Refactor web3 configuration to update ABI import paths

--- frontend/src/lib/web3Config.ts
+++ frontend/src/lib/web3Config.ts
@@ -1,6 +1,6 @@
 // @ts-nocheck
-import FactoryAbi from '../../../contracts/out/DistributionMarketFactory.sol/DistributionMarketFactory.json';
-import MarketAbi from '../../../contracts/out/DistributionMarket.sol/DistributionMarket.json';
+import FactoryAbi from '../assets/abis/DistributionMarketFactory.json';
+import MarketAbi from '../assets/abis/DistributionMarket.json';
 
 export const factoryAddress = '0x3B0B9918fda2A5E2b8b74Ed0662A1CeBD5aeA9ce';
 export const factoryAbi = FactoryAbi.abi;
